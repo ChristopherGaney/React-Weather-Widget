ffe0ce5dc0198f801d024a0012a192f46b640a68
Christopher Ganey <christophganey@gmail.com>
2016-12-23 18:45:04 -0600

    Format code

--- Component.js
+++ Component.js
@@ -11,10 +11,8 @@ module.exports = React.createClass({
 			lng: ''
 		};
 	},
-	componentDidMount: function() {
-		if(typeof window !== 'undefined'){
-			var locale = this.props.eventLocale;
-			locale = locale.trim().split(' ').join('+');
+	getLocale: function(locale) {
+		locale = locale.trim().split(' ').join('+');
 			var addr = "https://maps.googleapis.com/maps/api/geocode/json?address=" + locale;
 			axios
 				.get(addr)
@@ -25,23 +23,19 @@ module.exports = React.createClass({
 						this.setState({ lat: lt, lng: lg });
 					}
 				}.bind(this));
+	},
+	componentDidMount: function() {
+		if(typeof window !== 'undefined'){
+			var locale = this.props.eventLocale;
+			this.getLocale(locale);
 		}
 	},
 	componentWillReceiveProps: function(nextProps) {
+	console.log(nextProps);
 		if(this.props.eventLocale !== nextProps.eventLocale) {
 			var locale = nextProps.eventLocale;
-				locale = locale.trim().split(' ').join('+');
-				var addr = "https://maps.googleapis.com/maps/api/geocode/json?address=" + locale;
-				axios
-					.get(addr)
-					.then(function(result) {
-						if (result.status === 200) {
-							var lt = result.data.results[0].geometry.location.lat;
-							var lg = result.data.results[0].geometry.location.lng;
-							this.setState({ lat: lt, lng: lg });
-						}
-					}.bind(this));
-			}
+			this.getLocale(locale);
+		}	
 	  }, 
 	  
   
@@ -49,7 +43,15 @@ module.exports = React.createClass({
 		
         return (
             <div className="frameWrapper">
-                <iframe id="forecast_embed" key={this.state.lat} type="text/html" frameBorder="0" height="245" width="90%" src={"http://forecast.io/embed/#lat=" + this.state.lat + "&lon=" + this.state.lng + "&name=event"}> </iframe>
+                <iframe
+			id="forecast_embed"
+			key={this.state.lat}
+			type="text/html"
+			frameBorder="0"
+			height="245"
+			width="90%"
+			src={"http://forecast.io/embed/#lat=" + this.state.lat + "&lon=" + this.state.lng + "&name=event"}>
+		</iframe>
             </div>
         );
     }
