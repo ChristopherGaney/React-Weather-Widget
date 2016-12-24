359cc720ef7fb02a368d1147c1c536015993aee9
Christopher Ganey <christophganey@gmail.com>
2016-12-23 18:00:12 -0600

    React weather widget in an iframe

---
+++ Component.js
@@ -0,0 +1,56 @@
+"use strict";
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var axios = require('axios');
+	
+module.exports = React.createClass({
+	getInitialState: function() {
+		return {
+			lat: '',
+			lng: ''
+		};
+	},
+	componentDidMount: function() {
+		if(typeof window !== 'undefined'){
+			var locale = this.props.eventLocale;
+			locale = locale.trim().split(' ').join('+');
+			var addr = "https://maps.googleapis.com/maps/api/geocode/json?address=" + locale;
+			axios
+				.get(addr)
+				.then(function(result) {
+					if (result.status === 200) {
+						var lt = result.data.results[0].geometry.location.lat;
+						var lg = result.data.results[0].geometry.location.lng;
+						this.setState({ lat: lt, lng: lg });
+					}
+				}.bind(this));
+		}
+	},
+	componentWillReceiveProps: function(nextProps) {
+		if(this.props.eventLocale !== nextProps.eventLocale) {
+			var locale = nextProps.eventLocale;
+				locale = locale.trim().split(' ').join('+');
+				var addr = "https://maps.googleapis.com/maps/api/geocode/json?address=" + locale;
+				axios
+					.get(addr)
+					.then(function(result) {
+						if (result.status === 200) {
+							var lt = result.data.results[0].geometry.location.lat;
+							var lg = result.data.results[0].geometry.location.lng;
+							this.setState({ lat: lt, lng: lg });
+						}
+					}.bind(this));
+			}
+	  }, 
+	  
+  
+    render: function() {
+		
+        return (
+            <div className="frameWrapper">
+                <iframe id="forecast_embed" key={this.state.lat} type="text/html" frameBorder="0" height="245" width="90%" src={"http://forecast.io/embed/#lat=" + this.state.lat + "&lon=" + this.state.lng + "&name=event"}> </iframe>
+            </div>
+        );
+    }
+});
